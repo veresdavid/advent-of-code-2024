7a4e6852afaa779204ad280c2e136168128336a8
David Veres <veres.david11@gmail.com>
2024-12-05 08:51:14 +0100

    day 05 part 1 solution

---
+++ 05/print_queue_01.exs
@@ -0,0 +1,48 @@
+# Part 1 of the puzzle
+
+defmodule PrintQueue do
+  def solve do
+    [rule_input, update_input] =
+      IO.read(:eof)
+      |> String.split("\n\n", trime: true)
+      |> Enum.map(fn part -> String.split(part, "\n", trim: true) end)
+
+    rule_map =
+      rule_input
+      |> Map.new(fn rule -> {rule, :ok} end)
+
+    update_input
+    |> Enum.map(fn update -> String.split(update, ",") end)
+    |> Enum.filter(fn update -> valid_update?(update, rule_map) end)
+    |> Enum.map(&middle_element/1)
+    |> Enum.map(&String.to_integer/1)
+    |> Enum.sum()
+  end
+
+  defp valid_update?([], _) do
+    true
+  end
+
+  defp valid_update?([head | tail], rule_map) do
+    all_rule_matches =
+      tail
+      |> Enum.all?(fn element -> Map.has_key?(rule_map, "#{head}|#{element}") end)
+
+    if all_rule_matches do
+      valid_update?(tail, rule_map)
+    else
+      false
+    end
+  end
+
+  defp middle_element(list) do
+    middle_index =
+      length(list)
+      |> div(2)
+
+    Enum.at(list, middle_index)
+  end
+end
+
+PrintQueue.solve()
+|> IO.inspect()
